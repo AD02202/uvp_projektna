4eb3f1d8b1e9566f508be2d4bae086d0e6810508
AD02202 <ad1026@student.uni-lj.si>
2023-10-15 21:15:23 +0200

    Dodajanje komentarjev in urejanje po pep8

--- obdelaj.py
+++ obdelaj.py
@@ -7,18 +7,25 @@ class Sluzba():
     '''
     Razred sluzba predstavlja eno razpisano delovno mesto, vsaka sluzba ima ime, delodajalca, opis, kategorijo in regijo
     '''
+
     def __init__(self, ime, delodajalec, opis):
+        # nastavimo zacetne vrednosti razen kategorije in regije, ki jih nastavimo naknadno
         self.ime = ime
         self.delodajalec = delodajalec
         self.opis = opis
         self.kategorija = None
         self.regija = None
-        
 
     def __str__(self):
+        '''
+        Vrnemo niz, ki opisuje sluzbo
+        '''
         return f"SLUZBA ime: {self.ime}, delodajalec: {self.delodajalec}, opis: {self.opis})"
-    
-    def to_dict(self):
+
+    def pretvori_v_slovar(self):
+        '''
+        Vrnemo slovar, ki vsebuje polja sluzbe, ki ga lahko potem zapisemo v csv
+        '''
         return {"ime": self.ime, "delodajalec": self.delodajalec, "opis": self.opis, "kategorija": self.kategorija, "regija": self.regija}
 
 
@@ -35,86 +42,123 @@ def get_job_data(job_text):
     '''
     Ta funkcija iz bloka html kode izlusci vse potrebne podatke o sluzbi
     '''
+
+    # 1. IZLUSCIMO IME
     ime_vzorec = r'<h2 class="title">.*?</h2>'
     ime = re.search(ime_vzorec, job_text, flags=re.DOTALL).group()
+    # odstranimo html znacke
     ime = ime.strip('<h2 class="title">').strip('</h2>')
-    # odstranimo vejice, ker bomo shranjevali v csv
-    ime = ime.replace(',', ' ').replace('"', '').replace('\n', '').replace('(m/ž)', '').replace('m/ž', '')
-    
+    # odstranimo vejice, znake \n, narekovaje, ker bomo shranjevali v csv
+    ime = ime.replace(',', ' ').replace('"', '').replace(
+        '\n', '').replace('(m/ž)', '').replace('m/ž', '')
+
+    # 2. IZLUSCIMO OPIS
     opis_vzorec = r'<p class="premiumDescription">.*?</p>'
     opis = re.search(opis_vzorec, job_text, flags=re.DOTALL)
     if opis != None:
+        # odstranimo html znacke
         opis = opis.group().strip('<p class="premiumDescription">').strip('</p>')
+        # odstranimo nepotrebne znake
         opis = opis.replace(',', ' ').replace('"', '').replace('\n', '')
     else:
         # Ce oglas nima opisa, ga filtriramo
         return None
 
+    # 3. IZLUSCIMO DELODAJALCA
     delodajalec_vzorec = r'<div class="box-details-icon icon icon-home"></div>.*?<div class="detail">.*?</div>'
-    delodajalec = re.search(delodajalec_vzorec, job_text, flags=re.DOTALL).group()
-    delodajalec = delodajalec.strip('<div class="box-details-icon icon icon-home"></div>')
-    delodajalec = delodajalec.replace('\n', '').replace('\t', '').strip(' ')
-    delodajalec = delodajalec.strip('<div class="detail">').strip('</div>')
-    delodajalec = delodajalec.replace(',', ' ').replace('"', '').replace('\n', '')
-    
+    delodajalec = re.search(delodajalec_vzorec, job_text,
+                            flags=re.DOTALL).group()
+    # odstranimo html znacke
+    delodajalec = delodajalec.strip(
+        '<div class="box-details-icon icon icon-home"></div>').strip('</div>')
+    # odstranimo nepotrebne znake
+    delodajalec = delodajalec.replace('\n', '').replace(
+        '\t', '').strip(' ').replace(',', ' ').replace('"', '')
+
+    delodajalec = delodajalec.strip('<div class=detail>')
+    print(delodajalec)
+
     return Sluzba(ime, delodajalec, opis)
 
 
-def extract_jobs(text):
+def izvozi_sluzbe(text):
     '''
     Ta funckija iz html datoteke potegne bloke, ki predstavljajo posamezne sluzbe
     '''
     vzorec = r'<div class="w-inline-block job-ad top w-clearfix">.*?<a.*?>.*?</a>'
+    # bloki html kode, ki predstavljajo posamezno sluzbo
     sluzbe_texts = re.findall(vzorec, text, flags=re.DOTALL)
+    # seznam ki hrani vse sluzbe
     sluzbe = []
     for sluzba in sluzbe_texts:
         prebrana_sluzba = get_job_data(sluzba)
         if prebrana_sluzba != None:
-            sluzbe.append(prebrana_sluzba) 
+            sluzbe.append(prebrana_sluzba)
     return sluzbe
-   
+
 
 def load_category(path):
     '''
-    Ta funkcija nalozi celoten direktorij datotek oblike 1.html, 2.html, 3.html ... in iz njega
+    Ta funkcija nalozi celotno mapo datotek oblike 1.html, 2.html, 3.html ... in iz njega
     izlusci podatke o sluzbah
     '''
+    # seznam, ki hrani vse sluzbe
     sluzbe = []
     index = 1
+
+    # dokler obstajajo datoteke 1,2,3... .html jih nalagamo
     while os.path.exists(path + '/' + str(index) + '.html'):
         text = load_string_from_file(path + '/' + str(index) + '.html')
-        nove_sluzbe = extract_jobs(text)
+        # izvozi sluzbe
+        nove_sluzbe = izvozi_sluzbe(text)
         sluzbe.extend(nove_sluzbe)
         index += 1
 
     return sluzbe
 
 
-def write_csv(sluzbe, directory, filename, fieldnames):
-    os.makedirs(directory, exist_ok=True)
-    with open(directory + '/' + filename, "w", encoding='utf-8') as csv_file:
+def write_csv(sluzbe, mapa, filename, fieldnames):
+    '''
+    Ta funkcija zapise seznam sluzb v csv datoteko
+    '''
+
+    # naredi mapo, ce ne obstaja
+    os.makedirs(mapa, exist_ok=True)
+    # odpremo datoteko
+    with open(mapa + '/' + filename, "w", encoding='utf-8') as csv_file:
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        # zapisemo prvo vrstico z imeni polj
         writer.writeheader()
         for sluzba in sluzbe:
-            writer.writerow(sluzba.to_dict())
+            # zapisemo vsako vrstico
+            writer.writerow(sluzba.pretvori_v_slovar())
 
 
 def parse_data_to_csv():
-    kategorije = ["bancnistvo-finance", "matematika-fizika-in-naravoslovje", "upravljanje-svetovanje-vodenje"]
+    '''
+    Ta funkcija obdela shranjene podatke v /podatki in jih shrani v csv datoteko.
+    '''
+
+    kategorije = ["bancnistvo-finance",
+                  "matematika-fizika-in-naravoslovje", "upravljanje-svetovanje-vodenje"]
     regije = ["osrednjeslovenska", "obalna", "gorenjska"]
 
+    # seznam v katerem so vse sluzbe
     vse_sluzbe = []
     for kategorija in kategorije:
         for regija in regije:
             sluzbe = load_category("podatki/" + regija + "/" + kategorija)
-            
+
             for sluzba in sluzbe:
+                # nastavimo kategorijo in regijo
                 sluzba.kategorija = kategorija
                 sluzba.regija = regija
-            
+
             vse_sluzbe.extend(sluzbe)
-    
+
     print("Stevilo vseh sluzb", len(vse_sluzbe))
-    
+
+    # imena polj v csv datoteki
     fieldnames = ["ime", "delodajalec", "opis", "kategorija", "regija"]
+    # zapisemo sluzbe v csv datoteko
     write_csv(vse_sluzbe, "obdelani", "sluzbe.csv", fieldnames)
--- shrani.py
+++ shrani.py
@@ -7,31 +7,38 @@ def download_url_to_string(url):
     '''
     Ta funkcija nalozi vsebino spletne strani in jo vrne kot niz
     '''
+    # s try ujamemo napako, ce spletna stran ni dostopna
     try:
-        response = requests.get(url)
-        text = response.text
-    except requests.exceptions.RequestException: 
+        # poslji zahtevo na dani url in shrani text - html besedilo
+        text = requests.get(url).text
+
+    except requests.exceptions.RequestException:
         print('Ni mogoce najti spletne strani')
         return None
     return text
 
 
-def save_text_to_file(text, directory, filename):
+def save_text_to_file(text, mapa, filename):
     '''
-    Ta funkcija shrani podani niz v datoteko v ustrezni mapi
+    Ta funkcija shrani podani niz v datoteko v ustrezni mapi npr. podatki/gorenjska/bancnistvo-in-finance
+    filename pove ime datoteke npr. 1.html
     '''
-    os.makedirs(directory, exist_ok=True)
-    path = os.path.join(directory, filename)
+    # naredi mapo, ce se ne obstaja
+    os.makedirs(mapa, exist_ok=True)
+    # path je niz, v katerem se nahajata mapa in ime datoteke npr. podatki/gorenjska/bancnistvo-in-finance/1.html
+    path = os.path.join(mapa, filename)
     with open(path, 'w', encoding='utf-8') as file_out:
+        # shrani besedilo
         file_out.write(text)
     return None
 
 
 def contains_jobs(text):
     '''
-    Ta funkcija preveri, ali je v nizu html znacka za podatke o sluzbi
+    Ta funkcija z regularnimi izrazi (regex) preveri, ali je v nizu html znacka za podatke o sluzbi
     '''
     vzorec = r'<div class="w-inline-block job-ad top w-clearfix">.*?<a.*?>.*?</a>'
+    # vrnemo, ali smo nasli vzorec
     return bool(re.search(vzorec, text, flags=re.DOTALL))
 
 
@@ -40,21 +47,35 @@ def get_all_pages(osnovni_url, kategorija, regija):
     Ta funkcija v zanki nalaga nadaljne strani v doloceni kategoriji in regiji, dokler
     ji funkcija contains_jobs ne vrne, da stran ne vsebuje sluzb
     '''
+
+    # generiramo ustrezni url
     url = osnovni_url + "/" + kategorija + "/" + regija
+    # page steje, na kateri strani smo
     page = 1
+    # maximalno_st_strani, ki jih bomo prenesli
+    maximalno_st_strani = 10
+    # nalozimo prvo stran
     text = download_url_to_string(url + "?p=" + str(page))
-    
-    while contains_jobs(text) and page <= 10:
-        save_text_to_file(text, "podatki/" + regija + "/" + kategorija, str(page) + ".html")
+
+    # contains_jobs preveri, ali je spletna stran prazna (ne vsebuje nobenega oglasa)
+    while contains_jobs(text) and page <= maximalno_st_strani:
+        # primer mape: podatki/gorenjska/bancnistvo-finance, primer datoteke: 1.html
+        save_text_to_file(text, "podatki/" + regija + "/" +
+                          kategorija, str(page) + ".html")
         page += 1
         text = download_url_to_string(url + "?p=" + str(page))
 
 
 def save_web_data():
+    '''
+    Ta funkcija shrani podatke iz spleta in jih shrani v ustrezno datoteko na disku
+    '''
+
     link = "https://www.mojedelo.com/prosta-delovna-mesta"
-    kategorije = ["bancnistvo-finance", "matematika-fizika-in-naravoslovje", "upravljanje-svetovanje-vodenje"]
+    kategorije = ["bancnistvo-finance",
+                  "matematika-fizika-in-naravoslovje", "upravljanje-svetovanje-vodenje"]
     regije = ["osrednjeslovenska", "obalna", "gorenjska"]
 
     for kategorija in kategorije:
-            for regija in regije:
-                get_all_pages(link, kategorija, regija)    
+        for regija in regije:
+            get_all_pages(link, kategorija, regija)
